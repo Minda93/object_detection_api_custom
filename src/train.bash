94ea3fdb4e47d94c44e0f56955e4abedb5f45df3
minda93 <wwe421210w@gmail.com>
2020-03-06 13:11:01 +0000

    Add load pytorch model for readme

--- src/train.bash
+++ src/train.bash
@@ -4,9 +4,11 @@ MODEL_DIR="/workspace/minda/github/detect_ws/out/ssdlite_mobilenet_v2_fpn_512_r2
 WEIGHT_FILE="/workspace/minda/github/detect_ws/save_models/pytorch/ssdlite_mobilenet_v2_fpn_r2_anchor_3_bdd/weight_tf.pickle"
 LAYERS_FILE="/workspace/minda/github/detect_ws/save_models/pytorch/ssdlite_mobilenet_v2_fpn_r2_anchor_3_bdd/layer_name_custom.txt"
 
+mkdir -p logs/
+now=$(date +"%Y_%m_%d")
 python3 /workspace/minda/github/detect_ws/models/research/object_detection/legacy/train.py \
 --logtostderr \
---train_dir=${MODEL_DIR} \
+--train_dir=${MODEL_DIR} 2>&1 | tee logs/train_$now.txt \
 --pipeline_config_path=${CONFIG_FILE} \
 --pytorch_weight_path=${WEIGHT_FILE} \
 --pytorch_layers_path=${LAYERS_FILE} \
