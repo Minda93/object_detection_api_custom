a7c22cbe896e7da14cc7e2481538da67e69e56cf
minda93 <wwe421210w@gmail.com>
2020-04-01 16:25:50 +0000

    test success for other computer

--- src/train.bash
+++ src/train.bash
@@ -1,11 +1,10 @@
 # ssd mobilenet v2 fpn r2_anchor_3 bdd
 CONFIG_FILE="/workspace/minda/github/detect_ws/cfg/train/ssdlite_mobilenet_v2_fpn_512_r2_anchor_3_bdd.config"
-MODEL_DIR="/workspace/minda/github/detect_ws/out/ssdlite_mobilenet_v2_fpn_512_r2_anchor_3_bdd"
 WEIGHT_FILE="/workspace/minda/github/detect_ws/save_models/pytorch/ssdlite_mobilenet_v2_fpn6_r2_anchor_3_bdd/weight_tf_better.pickle"
 LAYERS_FILE="/workspace/minda/github/detect_ws/save_models/pytorch/ssdlite_mobilenet_v2_fpn6_r2_anchor_3_bdd/layer_name_custom_better.txt"
 
-# mkdir -p logs/
-# now=$(date +"%Y_%m_%d")
+MODEL_DIR="/workspace/minda/github/detect_ws/out/ssdlite_mobilenet_v2_fpn_512_r2_anchor_3_bdd"
+
 CUDA_VISIBLE_DEVICES=0 python3 /workspace/minda/github/detect_ws/models/research/object_detection/legacy/train.py \
 --logtostderr \
 --pipeline_config_path=${CONFIG_FILE} \
@@ -13,4 +12,3 @@ CUDA_VISIBLE_DEVICES=0 python3 /workspace/minda/github/detect_ws/models/research
 --pytorch_layers_path=${LAYERS_FILE} \
 --load_pytorch=True \
 --train_dir=${MODEL_DIR}
-# --train_dir=${MODEL_DIR} 2>&1 | tee logs/train_$now.txt
